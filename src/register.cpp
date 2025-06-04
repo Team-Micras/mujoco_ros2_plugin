8ed0ef2ceea35a725e55b5ded19cefb112259040
PedroDeSanti <phmartinsanti@gmail.com>
2025-06-04 05:44:45 +0200

    ✨ Add plugin registration
    Co-authored-by: Gabriel Cosme Barbosa <GabrielCosme@users.noreply.github.com>

---
+++ src/register.cpp
@@ -0,0 +1,9 @@
+#include <mujoco/mjplugin.h>
+#include "ros2.hpp"
+
+namespace mujoco::plugin::ros2 {
+mjPLUGIN_LIB_INIT {
+    ROS2Plugin::register_plugin();
+}
+
+}  // namespace mujoco::plugin::ros2
