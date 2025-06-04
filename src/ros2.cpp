c1cba43872e1f309a129a7bfb4e265015d835ed6
PedroDeSanti <phmartinsanti@gmail.com>
2025-06-04 06:24:54 +0200

    🔨  Update ROS2Plugin configuration handling and improve attribute reading functions
    Co-authored-by: Gabriel Cosme Barbosa <GabrielCosme@users.noreply.github.com>

--- src/ros2.cpp
+++ src/ros2.cpp
@@ -3,20 +3,44 @@
 
 #include "ros2.hpp"
 
-namespace mujoco::plugin::ros2 {
+namespace mujoco::plugin::ros2 { 
 
-constexpr char kAttrROSNamespace[] = "ros_namespace";
+static constexpr char attr_key_ros_namespace[] = "ros_namespace";
+static constexpr char attr_key_topic_queue_size[] = "topic_queue_size";
+
+int read_int_attr(const char* value, int default_value) {
+    if (value == nullptr || value[0] == '\0') {
+        return default_value;
+    }
+
+    return std::strtol(value, nullptr, 10);
+}
+
+std::string read_string_attr(const char* value, const std::string& default_value) {
+    if (value == nullptr) {
+        return default_value;
+    }
+
+    return std::string(value);
+}
 
 ROS2Plugin::Config ROS2Plugin::get_config_from_model(const mjModel* m, int instance) {
     Config config;
-    // config.ros_namespace = ReadOptionalStringAttr(m, instance, kAttrROSNamespace).value_or("mujoco/");
+    config.ros_namespace = read_string_attr(mj_getPluginConfig(m, instance, attr_key_ros_namespace), "mujoco/");
+    config.topic_queue_size = read_int_attr(mj_getPluginConfig(m, instance, attr_key_topic_queue_size), 1);
     return config;
 }
 
 ROS2Plugin::ROS2Plugin(const Config& config) {
     this->ros_namespace = config.ros_namespace;
-    rclcpp::init(0, nullptr);
+    this->topic_queue_size = config.topic_queue_size;
 
+    if (rclcpp::ok()) {
+        RCLCPP_INFO(rclcpp::get_logger("mujoco"), "ROS2 already initialized");
+        rclcpp::shutdown();
+    } 
+
+    rclcpp::init(0, nullptr);
     this->node = rclcpp::Node::make_shared("mujoco_ros2_plugin");
     RCLCPP_INFO(this->node->get_logger(), "ROS2 Mujoco Plugin node started");
 }
@@ -30,7 +54,7 @@ void ROS2Plugin::create_sensor_publishers(const mjModel* m) {
     RCLCPP_INFO(this->node->get_logger(), "Creating sensor publishers for %d sensors", m->nsensor);
 
     for (int i = 0; i < m->nsensor; i++) {
-        const char*       sensor_name = m->names + m->name_sensoradr[i];
+        const char* sensor_name = m->names + m->name_sensoradr[i];
         const std::string topic_name = this->ros_namespace + "sensor/" + std::string(sensor_name);
 
         int         pub_index;
@@ -38,11 +62,11 @@ void ROS2Plugin::create_sensor_publishers(const mjModel* m) {
         mjtDataType sensor_datatype = static_cast<mjtDataType>(m->sensor_datatype[i]);
 
         if (num_dimensions == 1) {
-            auto pub = this->node->create_publisher<example_interfaces::msg::Float64>(topic_name, 1);
+            auto pub = this->node->create_publisher<example_interfaces::msg::Float64>(topic_name, this->topic_queue_size);
             double_sensor_publishers.push_back(pub);
             pub_index = static_cast<int>(double_sensor_publishers.size() - 1);
         } else {
-            auto pub = this->node->create_publisher<example_interfaces::msg::Float64MultiArray>(topic_name, 1);
+            auto pub = this->node->create_publisher<example_interfaces::msg::Float64MultiArray>(topic_name, this->topic_queue_size);
             multiarray_sensor_publishers.push_back(pub);
             pub_index = static_cast<int>(multiarray_sensor_publishers.size() - 1);
         }
@@ -62,20 +86,19 @@ void ROS2Plugin::create_actuator_subscribers(const mjModel* m) {
         std::string topic_name = this->ros_namespace + "actuators/" + std::string(actuator_name) + "/command";
 
         auto sub = this->node->create_subscription<example_interfaces::msg::Float64>(
-            topic_name, 1, [](const example_interfaces::msg::Float64::SharedPtr msg) {}
+            topic_name, this->topic_queue_size, [](const example_interfaces::msg::Float64::SharedPtr msg) { }
         );
 
         actuator_subscribers.push_back(sub);
         this->actuators.push_back({i, static_cast<int>(actuator_subscribers.size() - 1)});
 
         RCLCPP_INFO(
-            this->node->get_logger(), "Created subscriber for actuator '%s' on topic '%s'", actuator_name,
-            topic_name.c_str()
+            this->node->get_logger(), "Created subscriber for actuator '%s' on topic '%s'", actuator_name, topic_name.c_str()
         );
     }
 }
 
-void ROS2Plugin::compute(const mjModel* m, mjData* d, int instance) {
+void ROS2Plugin::compute(const mjModel* m, mjData* d, int instance) {    
     if (not initialized) {
         this->create_sensor_publishers(m);
         this->create_actuator_subscribers(m);
@@ -105,7 +128,7 @@ void ROS2Plugin::compute(const mjModel* m, mjData* d, int instance) {
 
     for (auto& actuator : this->actuators) {
         example_interfaces::msg::Float64 msg;
-        rclcpp::MessageInfo              info;
+        rclcpp::MessageInfo               info;
 
         if (this->actuator_subscribers[actuator.comm_index]->take(msg, info)) {
             d->ctrl[actuator.object_index] = msg.data;
@@ -127,24 +150,22 @@ void ROS2Plugin::register_plugin() {
     mjpPlugin plugin;
     mjp_defaultPlugin(&plugin);
     plugin.name = "mujoco.ros2";
-
     plugin.capabilityflags |= mjPLUGIN_PASSIVE;
 
-    std::vector<const char*> attributes = {kAttrROSNamespace};
+    std::vector<const char*> attributes = {
+        attr_key_ros_namespace,
+        attr_key_topic_queue_size,
+    };
 
     plugin.nattribute = attributes.size();
     plugin.attributes = attributes.data();
-
-    plugin.nstate = +[](const mjModel* m, int instance) { return 0; };
+    plugin.nstate = +[](const mjModel* m, int instance) {
+        return 0;
+    };
 
     plugin.init = +[](const mjModel* m, mjData* d, int instance) {
-        std::fprintf(stderr, "Initializing ROS2 plugin for instance %d\n", instance);
-        if (d->plugin_data[instance] != 0) {
-            std::fprintf(stderr, "ROS2 plugin already initialized for instance %d\n", instance);
-        }
-
         Config config = get_config_from_model(m, instance);
-        auto*  ros2 = new ROS2Plugin(config);
+        auto* ros2 = new ROS2Plugin(config);
 
         if (ros2 == nullptr) {
             return -1;
@@ -152,10 +173,6 @@ void ROS2Plugin::register_plugin() {
 
         d->plugin_data[instance] = reinterpret_cast<uintptr_t>(ros2);
         return 0;
-    };
-
-    plugin.copy = +[](mjData* dest, const mjModel* m, const mjData* src, int instance) {
-        fprintf(stderr, "Copying plugin data is not implemented for ROS2 plugin.\n");
     };
 
     plugin.reset = +[](const mjModel* m, mjtNum* plugin_state, void* plugin_data, int instance) {
